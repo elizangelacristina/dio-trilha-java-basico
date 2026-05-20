d60938824dec4a0b74887e2582bcdd59e390592e
elizangelacristina <elizangelacristina@ufpr.br>
2026-05-20 09:13:04 -0300

    Ajuste para fechar a variável scanner.

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -7,27 +7,28 @@ public class ContaTerminal {
     private static double  saldo;
 
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        String strSaldo;
-        try {
-            System.out.print("Por favor, digite o número da Conta ! ");
-            numero = sc.nextInt();
-            System.out.print("Por favor, digite o número da Agência ! ");
-            agencia = sc.next();
-            System.out.print("Por favor, digite o nome do Cliente ! ");
-            nomeCliente = sc.next();
-            System.out.print("Por favor, digite o saldo ! ");
-            strSaldo = sc.next();
-            saldo = Double.parseDouble(strSaldo);
+        try (Scanner sc = new Scanner(System.in)) {
+            String strSaldo;
+            try {
+                System.out.print("Por favor, digite o número da Conta ! ");
+                numero = sc.nextInt();
+                System.out.print("Por favor, digite o número da Agência ! ");
+                agencia = sc.next();
+                System.out.print("Por favor, digite o nome do Cliente ! ");
+                nomeCliente = sc.next();
+                System.out.print("Por favor, digite o saldo ! ");
+                strSaldo = sc.next();
+                saldo = Double.parseDouble(strSaldo);
 
-            System.out.print("Olá " + nomeCliente + ", ");
-            System.out.print("obrigado por criar uma conta em nosso banco, ");
-            System.out.print("sua agência é "+ agencia +", conta " + numero + " ");
-            System.out.print("e seu saldo "+ saldo +" já está disponível para saque.");
-        } catch (Exception e) {
-            System.out.println("Erro: " + e.getMessage());
+                System.out.print("Olá " + nomeCliente + ", ");
+                System.out.print("obrigado por criar uma conta em nosso banco, ");
+                System.out.print("sua agência é "+ agencia +", conta " + numero + " ");
+                System.out.print("e seu saldo "+ saldo +" já está disponível para saque.");
+            } catch (NumberFormatException e) {
+                System.out.println("Erro: " + e.getMessage());
+            }
+            sc.close();
         }
-
     }
 
 }
