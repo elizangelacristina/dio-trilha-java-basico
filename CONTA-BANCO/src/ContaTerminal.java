30cff6316fb5f59a9851e1b477c98895d45d3894
elizangelacristina <elizangelacristina@ufpr.br>
2026-05-20 09:15:50 -0300

    Arrumado espaços.

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -21,8 +21,8 @@ public class ContaTerminal {
                 saldo = Double.parseDouble(strSaldo);
                 System.out.print("Olá " + nomeCliente + ", ");
                 System.out.print("obrigado por criar uma conta em nosso banco, ");
-                System.out.print("sua agência é "+ agencia +", conta " + numero + " ");
-                System.out.print("e seu saldo "+ saldo +" já está disponível para saque.");
+                System.out.print("sua agência é " + agencia + ", conta " + numero + " ");
+                System.out.print("e seu saldo " + saldo + " já está disponível para saque.");
             } catch (NumberFormatException e) {
                 System.out.println("Erro: " + e.getMessage());
             }
