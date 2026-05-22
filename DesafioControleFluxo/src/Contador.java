2f46cd2a76a8c0ea52209ca3b91cf1c8c3b6520a
elizangelacristina <elizangelacristina@ufpr.br>
2026-05-22 12:11:17 -0300

    Retirado espaços.

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -19,9 +19,7 @@ public class Contador {
 		if (parametroUm > parametroDois) {
 			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
 		}
-
 		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
         for(int i = 0; i < contagem; i++) {
             System.out.println("Imprimindo o número: " +(i + 1));
         }
--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,12 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
 class ParametrosInvalidosException extends Exception {
     public ParametrosInvalidosException(String message) {
         super(message);
     }
-
 }
